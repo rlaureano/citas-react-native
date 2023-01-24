a0d8586e92542bb0d7f591fe82a9b46c28256e71
Raul <ing.luareano.jar@gmail.com>
2023-01-23 22:43:05 -0600

    styles title

---
+++ App.js
@@ -0,0 +1,56 @@
+/**
+ * Sample React Native App
+ * https://github.com/facebook/react-native
+ *
+ * @format
+ * @flow strict-local
+ */
+
+import React from 'react';
+
+import {
+  SafeAreaView,
+  ScrollView,
+  StatusBar,
+  StyleSheet,
+  Text,
+  useColorScheme,
+  View,
+} from 'react-native';
+
+
+/* $FlowFixMe[missing-local-annot] The type annotation(s) required by Flow's
+ * LTI update could not be added via codemod */
+
+
+const App = () => {
+
+  
+
+  return (
+    <SafeAreaView style={styles.container}> 
+      <Text style={styles.title}> Administrador de citas {' '}
+        <Text style={styles.titleBold}>Veterinaria</Text>
+      </Text>
+    </SafeAreaView>
+  );
+};
+
+const styles = StyleSheet.create({
+  container: {
+    backgroundColor: '#F3F4F6',
+    flex: 1
+  },
+  title: {
+    textAlign: 'center',
+    fontSize: 30,
+    color: '#374151',
+    fontWeight: '600'
+  },
+  titleBold: {
+    fontWeight: '900',
+    color: '#6D28D9'
+  }
+});
+
+export default App;
